8e6ed7df37d6bdcc81fa89399e6982cd56240c78
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-10-04 21:13:55 +0300

    fix validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,6 @@ class Task < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :author, presence: true
+  validates :name, :description, :author, presence: true
   validates :description, length: { maximum: 500 }
 end

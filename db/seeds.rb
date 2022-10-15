ca37c2ff4137a466237227e5be6c64beda5793dc
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-10-15 20:41:37 +0300

    add faker and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,29 @@ admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 
 admin.password = 'admin'
 admin.save
 
-60.times do |i|
+60.times do
   u = [Manager, Developer].sample.new
-  u.email = "email#{i}@mail.gen"
-  u.first_name = "FN#{i}"
-  u.last_name = "LN#{i}"
-  u.password = "#{i}"
+  u.first_name = Faker::Name.first_name
+  u.last_name = Faker::Name.last_name
+  u.email = Faker::Internet.email(name: "#{u.first_name} #{u.last_name}", separators: '.')
+  u.password = 'default'
   u.save
 end
+
+70.times do
+    t = Task.new
+    t.name = Faker::Hacker.noun.capitalize
+    t.description = Faker::Hacker.say_something_smart
+    t.author = User.where(type: "Manager").sample
+    t.assignee = User.where(type: "Developer").sample
+    t.state = ['new_task',
+               'in_development',
+               'in_qa',
+               'in_code_review',
+               'ready_for_release',
+               'released',
+               'archived',
+              ].sample
+    t.expired_at = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now + 30)
+    t.save
+  end

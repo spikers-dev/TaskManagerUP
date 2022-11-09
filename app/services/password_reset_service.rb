84cdc6886313297390246ac144ac952dc7c196d0
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-11-09 22:40:04 +0300

    fix PasswordResetService

--- app/services/password_reset_service.rb
+++ app/services/password_reset_service.rb
@@ -1,21 +1,23 @@
 module PasswordResetService
-  def self.create_password_reset_token!(user)
-    user.password_reset_token = generate_password_reset_token
-    user.password_reset_expires_at = Time.zone.now + 1.days
-    user.save!
-  end
+  class << self
+    def create_password_reset_token!(user)
+      user.password_reset_token = generate_password_reset_token
+      user.password_reset_expires_at = Time.zone.now + 1.days
+      user.save!
+    end
 
-  def self.generate_password_reset_token
-    SecureRandom.urlsafe_base64
-  end
+    def generate_password_reset_token
+      SecureRandom.urlsafe_base64
+    end
 
-  def self.password_token_validation?(user)
-    user.password_reset_expires_at < Date.today
-  end
+    def password_token_validation?(user)
+      user.password_reset_expires_at < Date.today
+    end
 
-  def self.reset_password!(user)
-    user.password_reset_token = nil
-    user.password_reset_expires_at = nil
-    user.save!
+    def reset_password!(user)
+      user.password_reset_token = nil
+      user.password_reset_expires_at = nil
+      user.save!
+    end
   end
 end

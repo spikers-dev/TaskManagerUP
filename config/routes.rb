c6606bef337e7dd9e0a81f19de1dc971239b7bd9
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-11-10 15:47:13 +0300

    launch and debugging sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   default_url_options :host => ENV['HOST']
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?  
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount Sidekiq::Web => '/admin/sidekiq'
   
   root :to => "web/boards#show"
 

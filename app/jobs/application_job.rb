cd1f96580aa5888408d10bcbca61112aa15ca1bb
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-11-10 21:24:24 +0300

    set uniqueness

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,5 @@ class ApplicationJob
 
   sidekiq_options queue: :mailers
   sidekiq_throttle_as :mailer
+  sidekiq_options lock: :until_and_while_executing, on_conflict: { client: :log, server: :reject }
 end

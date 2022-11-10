0d3748a8f6773fc1e14042a828ed45565322eb5d
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-11-10 15:10:07 +0300

    config sidekiq

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,6 +13,7 @@ end
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'sidekiq/testing'
 
 class ActiveSupport::TestCase
   include AuthHelper
